46bce03d2653ca0a9349dfb10aae466ff593f510
claradelvecchio <clara.anquetil@studentfr.ch>
2025-05-15 09:14:51 +0200

    meh

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
 import gsap from "gsap";
 gsap.to(".first", { rotationZ: 360, duration: 50, repeat: -1 });
 gsap.to(".two", { rotationZ: 360, duration: 200, repeat: -1 });
-gsap.to(".three", { rotationZ: -360, duration: 300, repeat: -1 });
+gsap.to(".three", { rotationZ: -360, duration: 100, repeat: -1 });
